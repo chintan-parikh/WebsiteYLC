fd5f1e303888c21c5bdf37ab217d5f1a0dec2213
girishramnani <girishramnani95@gmail.com>
2016-06-12 13:40:57 +0530

    initial working completed, refinement left

--- js/pageSlider.js
+++ js/pageSlider.js
@@ -8,12 +8,32 @@ function PageSlider(options) {
 
 }
 
+PageSlider.prototype.eventListener = function (evt) {
+
+    var id = evt.target.attributes.id;
+    this.show(id);
+};
+
+PageSlider.prototype.show = function (id) {
+    var container = this.findContainer(id);
+    this.hideAll()
+    container.show();
+};
+
+PageSlider.prototype.attachToAll = function () {
+    var self = this;
+    for(var i=0;i<this.nav.length;i++){
+        $(this.nav[i]).on("click",this.eventListener.bind(self));
+    }
+
+};
+
 PageSlider.prototype.identify = function () {
     var containers = $(this.sliderContent).children();
     this.containers = containers.filter(function(index){
         // only support direct div child for now
         return containers[index].tagName =="DIV";
-    })
+    });
     this.nav = $(this.sliderNav).find("a");
 
 };
@@ -21,14 +41,42 @@ PageSlider.prototype.identify = function () {
 PageSlider.prototype.initialize = function () {
     this.identify();
     this.validate();
+    this.attachToAll();
+};
+
+PageSlider.prototype.findContainer = function (id) {
+
+    for(var i =0 ;i < this.containers.length;i++){
+        if( $(this.containers[i]).attr("id") == id ){
+            return $(this.containers[i]);
+        }
+    }
+
+};
+
+// Is used to make each container cover the whole page
+PageSlider.prototype.maximizeAll = function () {
+
+};
+PageSlider.prototype.hideAll = function () {
+    var self = this;
+    this.containers.map(function(index) {
+        $(this).hide();
+    });
 
 
 };
+PageSlider.prototype.moveTo = function (id) {
+
+};
+
 PageSlider.prototype.validate = function () {
 
     if(this.nav.length != this.containers.length){
         throw new Error("the link count and container count dont match");
     }
+
+
 
 
 };
